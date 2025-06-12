d4a4d8783d58b9329987d5e713d8c8a877dd0b5e
Luis Romano <luisromano@gmail.com>
2025-06-12 18:46:11 -0300

    mas vistas y urls

--- crud/persona/urls.py
+++ crud/persona/urls.py
@@ -9,4 +9,24 @@ urlpatterns = [
         PersonaListView.as_view(),
         name='lista'
     ),
+    path(
+        'detalle/<int:pk>/',
+        PersonaDetailView.as_view(),
+        name='detalle'
+    ),
+    path(
+        'crear/',
+        PersonaCreateView.as_view(),
+        name='crear'
+    ),
+    path(
+        'editar/<int:pk>/',
+        PersonaUpdateView.as_view(),
+        name='editar'
+    ),
+    path(
+        'eliminar/<int:pk>/',
+        PersonaDeleteView.as_view(),
+        name='eliminar'
+    ),
 ]
--- crud/persona/views.py
+++ crud/persona/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 from .models import Persona
 
 
@@ -8,5 +9,31 @@ class PersonaListView(ListView):
     template_name = "persona/lista.html"
     context_object_name = "personas"
 
+class PersonaDetailView(DetailView):
+    model = Persona
+    template_name = "persona/detalle.html"
+    context_object_name = "persona"
 
+class PersonaCreateView(CreateView):
+    model = Persona
+    template_name = "persona/crear.html"
+    fields = ['nombre', 'edad', 'email']
+    success_url = reverse_lazy('persona:lista')
 
+class PersonaUpdateView(UpdateView):
+    model = Persona
+    template_name = "persona/crear.html"
+    fields = ['nombre', 'edad', 'email']
+    success_url = reverse_lazy('persona:lista')
+
+class PersonaDeleteView(DeleteView):
+    model = Persona
+    template_name = "persona/eliminar.html"
+    context_object_name = "persona"
+    success_url = reverse_lazy('persona:lista')
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['action'] = 'eliminar'
+        return context
+# This view is used to delete a Persona instance.
